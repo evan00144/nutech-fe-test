6f9c7cc86f5e4c476fd3b60538e2467df8db6d61
evan00144 <evan2@student.umn.ac.id>
2023-09-08 13:48:27 +0700

    transaction page

--- src/pages/TransactionPage.tsx
+++ src/pages/TransactionPage.tsx
@@ -1,18 +1,84 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import AccountInfo from "../components/AccountInfo";
+import { fetchData } from "../service/service";
 
 export default function TransactionPage() {
+  const [transactions, setTransactions] = useState([]);
+  const [page, setPage] = useState(0);
+  const getTransaction = useCallback(async () => {
+    try {
+      const res = await fetchData(
+        `https://take-home-test-api.nutech-integrasi.app/transaction/history?offset=${page}&limit=5`
+      );
+      const data = await res.json();
+      setTransactions(data?.data?.records);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    getTransaction();
+  }, [getTransaction, page]);
+
+  const getDate = (string: string) => {
+    const inputDateStr = string;
+    const date = new Date(inputDateStr);
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = String(date.getFullYear());
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    const day = String(date.getDate()).padStart(2, "0");
+
+    const formattedDateStr = `${day} ${month} ${year} ${hours}:${minutes} WIB`;
+    return formattedDateStr;
+  };
   return (
     <>
       <AccountInfo />
       <h5 className="fw-bold mb-4">Semua Transaksi</h5>
-
-      <div className="card-transaksi">
-        <div>
-          <div className="jumlah">10</div>
-          <div className="tanggal">20 agustus</div>
+      {transactions?.map((e: any, index) => (
+        <div className="card-transaksi mb-3" key={index}>
+          <div>
+            <div
+              className={`jumlah ${
+                e?.transaction_type === "PAYMENT" ? "red" : ""
+              }`}
+            >
+              {e?.transaction_type === "PAYMENT" ? "-" : "+"} Rp.
+              {e?.total_amount}
+            </div>
+            <div className="tanggal">{getDate(e?.created_on)}</div>
+          </div>
+          <span
+            style={{
+              fontSize: ".8rem",
+            }}
+          >
+            {e?.description}
+          </span>
         </div>
-        <span>jenis transaksi</span>
+      ))}
+      <div
+        onClick={() => setPage((prev) => prev+1)}
+        className="text-center cursor-pointer text-primary"
+      >
+        Show More
       </div>
     </>
   );
